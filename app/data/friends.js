de329e0f4ccc71261f10055b49597ceb1be31a78
tcknapp <trevor.knapp.1991@gmail.com>
2018-09-03 18:20:57 -0700

    survey js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,40 @@
-// You should save your application's data inside of app/data/friends.js as an array of objects. Each of these objects should roughly follow the format below.
+// You should save your application's data inside of app/data/friends.js as an array of objects. 
+// Each of these objects should roughly follow the format below.
 
 // {
 //   "name":"Ahmed",
 //   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-//   "scores":[
-//       5,
-//       1,
-//       4,
-//       4,
-//       5,
-//       1,
-//       2,
-//       5,
-//       4,
-//       1
-//     ]
+//   "scores":[ 5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
 // }
+
+var friends = [
+   
+    {
+        name:"Joe Bob",
+        photo:"https://pbs.twimg.com/profile_images/440961110750281728/mV8Nb4DD.jpeg",
+        scores:[5,3,3,3,1,3,5,4,2,1],
+    },
+    {
+        name:"",
+        photo:"",
+        scores:[],
+    },
+    {
+        name:"",
+        photo:"",
+        scores:[],
+    },
+    {
+        name:"",
+        photo:"",
+        scores:[],
+    },
+    {
+        name:"",
+        photo:"",
+        scores:[],
+    }
+    
+];
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //Required
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require("path");
 
 var app = express();
 
@@ -11,13 +12,16 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.text());
+//For JSON API
+app.use(bodyParser.json({ type: "application/vnd.api+json"}));
 
 
 //Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
-//Listener - starts server
+//Listener - 'starts' server
 app.listen(PORT, function() {
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
