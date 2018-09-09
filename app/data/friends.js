40c6e0367d9cdeb6e2396f3967a7862e4e18b1d7
tcknapp <trevor.knapp.1991@gmail.com>
2018-09-08 18:09:46 -0700

    update friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -10,28 +10,28 @@
 var friends = [
    
     {
-        name:"Joe Bob",
+        name:"Rob Gronkowski",
         photo:"https://pbs.twimg.com/profile_images/440961110750281728/mV8Nb4DD.jpeg",
         scores:[5,3,3,3,1,3,5,4,2,1],
     },
     {
-        name:"Bobby Ray",
-        photo:"https://pbs.twimg.com/profile_images/440961110750281728/mV8Nb4DD.jpeg",
+        name:"Joey Tribbiani",
+        photo:"https://imgix.bustle.com/uploads/image/2017/8/22/4dbabff7-c8bd-4817-abff-a413fd946e49-joey-tribbiani-pineapple.jpg?w=375&h=225&fit=crop&crop=faces&auto=format&q=70&dpr=2",
         scores:[2,1,2,1,2,1,2,1,2,1],
     },
     {
-        name:"Bill Bobert",
-        photo:"https://pbs.twimg.com/profile_images/440961110750281728/mV8Nb4DD.jpeg",
+        name:"Shia Labeouf",
+        photo:"http://assets-s3.usmagazine.com/uploads/assets/articles/83841-shia-labeouf-rattail-braid-hairstyle-photos/1425398622_shia-labeouf-zoom.jpg",
         scores:[4,4,4,3,4,3,4,3,5,4],
     },
     {
-        name:"Bobette Ray",
-        photo:"https://pbs.twimg.com/profile_images/440961110750281728/mV8Nb4DD.jpeg",
+        name:"Post Malone",
+        photo:"https://media.thetab.com/blogs.dir/279/files/2018/06/hand-person-people-human.jpg",
         scores:[1,2,3,4,5,4,3,2,1,2],
     },
     {
-        name:"Bo Johnson",
-        photo:"https://pbs.twimg.com/profile_images/440961110750281728/mV8Nb4DD.jpeg",
+        name:"Chad Kroeger",
+        photo:"http://www1.pictures.gi.zimbio.com/Chad+Kroeger+2006+American+Music+Awards+Arrivals+RsOv9jCIw7hl.jpg",
         scores:[5,5,5,5,5,5,5,5,5,5],
     }
     
